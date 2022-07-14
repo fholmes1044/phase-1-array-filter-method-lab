053ec9e820900eee3b22e5620a4207bbf7577f17
+ <+>
2022-07-13 23:28:55 -0400

    commit tree 0636a62bd16921e2bc72f34f23c270b3d26725d0

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(array, string){
+   return array.filter(name => name.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch (array, string){
+    return array.filter(name => name.toLowerCase().indexOf(string.toLowerCase())=== 0)
+}
+
+function matchName(array, string){
+return array.filter(driver => driver.name === string )
+}
